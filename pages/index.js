d74afd182d16d58aff4e79573f523b45f26b087b
oNare <onare1992@gmail.com>
2022-03-23 11:48:01 -0400

    NextJS Course w React

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ function HomePage(props) {
       <Head>
         <title>React Meetups</title>
       </Head>
-      <MeetupList meetups={props.meetups} />;
+      <MeetupList meetups={props.meetups} />
     </Fragment>
   );
 }
