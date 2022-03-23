afc9779cf492734195d76188685b9690c522a3fb
oNare <onare1992@gmail.com>
2022-03-23 11:20:32 -0400

    NextJS Course w React

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
+import MeetupList from '../components/meetups/MeetupList';
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+// export function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return { props: { meetups: DUMMY_MEETUPS } };
+// }
+
+export async function getStaticProps() {
+  // fetch data from API
+  const client = await MongoClient.connect(
+    'mongodb+srv://onare:WiMsduI0Ilhoc0GT@cluster0.ng3yd.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,36 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addNewMeetupHandler(enteredData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+      </Head>
+
+      <NewMeetupForm onAddMeetup={addNewMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
